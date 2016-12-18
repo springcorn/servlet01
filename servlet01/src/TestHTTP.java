1760527193a2e98d8f23777583482b52bad3b00d
springcorn <liupengyan0823@163.com>
2016-12-18 19:14:31 +0800

    第一次提交

---
+++ servlet01/src/TestHTTP.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+
+public class TestHTTP {
+	public static void main(String[] args) throws Exception {
+		Socket s = new Socket("www.baidu.com", 80);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+		bw.write("OPTIONS / HTTP/1.1");
+		bw.newLine();
+		bw.write("Host: www.baidu.com:80");
+		bw.newLine();
+		bw.write("Content-Type: text/html");
+		bw.newLine();
+		bw.newLine();
+		bw.flush();
+		BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+		String str = null;
+		while((str = br.readLine()) != null) {
+			System.out.println(str);
+		}
+		bw.close();
+		br.close();
+		s.close();
+	}
+}
